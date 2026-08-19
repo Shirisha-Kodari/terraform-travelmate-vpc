9484a10f3eb57a4da5fee6be63f27bdb132a325d
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-08-19 19:30:31 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@
 # EXISTING DEFAULT VPC
 # ============================================================
 
-data "aws_vpc" "default" {      
+data "aws_vpc" "default" {       
   default = true  
 }
 
